84390455c8bb0c23b822f791d13ecaa86777079a
xrswyea <cjy1943222010@qq.com>
2021-05-14 21:45:22 +0800

    添加项目文件。

---
+++ 轻松记账/FormMain.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace 轻松记账
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.verificationComponent1 = new HZH_Controls.Controls.VerificationComponent(this.components);
+            this.tabControlExt1 = new HZH_Controls.Controls.TabControlExt();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabControlExt1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // verificationComponent1
+            // 
+            this.verificationComponent1.AutoCloseErrorTipsTime = 3000;
+            this.verificationComponent1.ErrorTipsBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(77)))), ((int)(((byte)(58)))));
+            this.verificationComponent1.ErrorTipsForeColor = System.Drawing.Color.White;
+            // 
+            // tabControlExt1
+            // 
+            this.tabControlExt1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this.tabControlExt1.CloseBtnColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(85)))), ((int)(((byte)(51)))));
+            this.tabControlExt1.Controls.Add(this.tabPage1);
+            this.tabControlExt1.Controls.Add(this.tabPage2);
+            this.tabControlExt1.Controls.Add(this.tabPage3);
+            this.tabControlExt1.Controls.Add(this.tabPage4);
+            this.tabControlExt1.IsShowCloseBtn = false;
+            this.tabControlExt1.ItemSize = new System.Drawing.Size(0, 50);
+            this.tabControlExt1.Location = new System.Drawing.Point(6, 67);
+            this.tabControlExt1.Name = "tabControlExt1";
+            this.tabControlExt1.SelectedIndex = 0;
+            this.tabControlExt1.Size = new System.Drawing.Size(794, 381);
+            this.tabControlExt1.TabIndex = 7;
+            this.tabControlExt1.UncloseTabIndexs = null;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(786, 323);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "记账";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(786, 323);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "账单";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 4);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(786, 323);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "统计";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 4);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(786, 323);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "设置";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControlExt1);
+            this.IsShowCloseBtn = true;
+            this.Name = "FormMain";
+            this.Text = "Form1";
+            this.Title = "轻松记账";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Controls.SetChildIndex(this.tabControlExt1, 0);
+            this.tabControlExt1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private HZH_Controls.Controls.VerificationComponent verificationComponent1;
+        private HZH_Controls.Controls.TabControlExt tabControlExt1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+    }
+}
+
---
+++ 轻松记账/FormMain.cs
@@ -0,0 +1,59 @@
+﻿using HZH_Controls.Controls;
+using HZH_Controls.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 轻松记账
+{
+    public partial class FormMain : FrmWithTitle
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            for (; ; )
+            {
+                FrmInputs frm = new FrmInputs("请输入密码",
+                                       new string[] { "密码" });
+                DialogResult rst = frm.ShowDialog(this);
+
+                Console.WriteLine(frm.Values[0]);
+                //frm.ShowDialog(this);
+                if (frm.Values[0] == "123")
+                {
+                    FrmTips.ShowTipsSuccess(this, "密码正确");
+                    break;
+                }
+                else
+                {
+                    if (rst==DialogResult.Cancel)
+                    {
+                        this.Close();
+                        this.Dispose();
+                        break;
+                    }
+
+                    FrmTips.ShowTipsWarning(this, "密码错误");
+                    
+              
+                }
+            
+
+
+
+            }
+
+        }
+    }
+}
